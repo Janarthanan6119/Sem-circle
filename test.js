45d480a2ff04d7a81aa7d047ec29605981179380
Janarthanan6119 <91175755+Janarthanan6119@users.noreply.github.com>
2023-05-19 12:09:34 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const httpMocks = require("node-mocks-http");
 const exampleRouteHandler = require("./example-router");
 describe("Example Test 1", () => {
-  it("should return 'Im Jana Earthling!' for GET /example", () => {
+  it("should return 'hi Im Earthling!' for GET /example", () => {
     const mockRequest = httpMocks.createRequest({
       method: "GET",
       url: "/example"
